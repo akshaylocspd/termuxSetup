b4f47afccbed3dd467bc92326b1da58652cf7fb5
akshay <akshayvairagade80@gmail.com>
2023-04-20 23:27:35 +0530

    termux full setup using single file

---
+++ fullSetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update package list
+apt update && upgrade -y
+
+# Install packages using apt
+apt install -y openssh nano dlib python nodejs git curl wget python-pip python-numpy yarn
+#python-pillow
+
+# Install http-server using npm
+npm install -g http-server
+
+# Install wheel and setuptools using pip
+pip install wheel setuptools
+
+# Install Python packages using pip
+pip install beautifulsoup4==4.12.0 bs4==0.0.1 face-recognition==1.3.0 Flask==2.2.2 pymongo==4.3.3 pypng==0.20220715.0 PyQRCode==1.2.1 requests==2.28.1 selenium==4.8.3 urllib3==1.26.15 webdriver-manager==3.8.5
+
+# Output: Display completion message
+echo "All packages have been installed successfully!"
